435d2e6aded4fe67313887840652426e1c826fc8
nicholaspei <nic162534@gmail.com>
2016-10-20 14:10:28 +0800

    move project to root dictionary

---
+++ CommonPool2.Test/StringBufferFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using CommonPool2.impl;
+
+namespace CommonPool2.Test
+{
+    public class StringBufferFactory:BasePooledObjectFactory<StringBuilder>
+    {
+        public override StringBuilder Create()
+        {
+           return new StringBuilder();
+        }
+
+        public override IPooledObject<StringBuilder> Wrap(StringBuilder obj)
+        {
+            return new DefaultPooledObject<StringBuilder>(obj);
+        }
+
+        /// <summary>
+        /// Clear the stringbuilder
+        /// </summary>
+        /// <param name="pooledObject"></param>
+        public override void PassivateObject(IPooledObject<StringBuilder> pooledObject)
+        {
+            pooledObject.GetObject().Clear();
+        }
+    }
+}
